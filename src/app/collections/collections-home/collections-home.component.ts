49ceabde830e7f7f24adb1be3321baae9b02974b
AkhlakurRahman <akhlakur.rahmann@gmail.com>
2020-04-03 12:17:37 +0600

    table added with reusable component

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-collections-home',
-  templateUrl: './collections-home.component.html',
-  styleUrls: ['./collections-home.component.css']
+  selector: "app-collections-home",
+  templateUrl: "./collections-home.component.html",
+  styleUrls: ["./collections-home.component.css"]
 })
 export class CollectionsHomeComponent implements OnInit {
+  data = [
+    { name: "Rabby", age: 27, occupation: "Engineer" },
+    { name: "John Doe", age: 24, occupation: "Designer" },
+    { name: "Jane Doe", age: 31, occupation: "Animator" }
+  ];
 
-  constructor() { }
+  headers = [
+    { key: "name", label: "Name" },
+    { key: "age", label: "Age" },
+    { key: "occupation", label: "Occupation" }
+  ];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
